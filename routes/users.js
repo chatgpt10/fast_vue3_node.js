ee4fb4cc77417e4989a2113084ce1f1c4a5286a8
liuerjian <1913698873@qq.com>
2023-02-16 17:56:05 +0800

    faet:完成用户的增删改查

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,49 @@ const router = require("koa-router")();
 const { User } = require("../models");
 router.prefix("/users");
 
+// 查询所有用户
+router.get("/list", async (ctx) => {
+  await User.find()
+    .then((res) => {
+      if (res) {
+        ctx.body = {
+          code: 200,
+          msg: "查询成功",
+          data: res,
+        };
+      } else {
+        ctx.body = {
+          code: 400,
+          msg: "查询失败",
+        };
+      }
+    })
+    .catch((error) => {
+      console.error("查询出现异常", error);
+    });
+});
+// 查询用户
+router.get("/user/:id", async (ctx) => {
+  let { id } = ctx.params;
+  await User.findOne({ _id: id })
+    .then((res) => {
+      if (res) {
+        ctx.body = {
+          code: 200,
+          msg: "查询成功",
+          data: res,
+        };
+      } else {
+        ctx.body = {
+          code: 400,
+          msg: "查询失败",
+        };
+      }
+    })
+    .catch((error) => {
+      console.error("查询出现异常", error);
+    });
+});
 // 添加用户
 router.post("/add", async (ctx) => {
   let { username, pwd } = ctx.request.body;
@@ -29,4 +72,59 @@ router.post("/add", async (ctx) => {
     });
 });
 
+// 用户信息修改
+router.post("/update", async (ctx) => {
+  let { _id, username, pwd } = ctx.request.body;
+  await User.updateOne(
+    {
+      _id: _id,
+    },
+    {
+      username: username,
+      pwd: pwd,
+    }
+  )
+    .then((res) => {
+      if (res) {
+        ctx.body = {
+          code: 200,
+          msg: "修改成功",
+          data: res,
+        };
+      } else {
+        ctx.body = {
+          code: 400,
+          msg: "修改失败",
+        };
+      }
+    })
+    .catch((error) => {
+      console.error("修改出现异常", error);
+    });
+});
+
+// 删除用户
+router.delete("/del", async (ctx) => {
+  let { _id } = ctx.request.body;
+  await User.findOneAndDelete({
+    _id: _id,
+  })
+    .then((res) => {
+      if (res) {
+        ctx.body = {
+          code: 200,
+          msg: "删除成功",
+          data: res,
+        };
+      } else {
+        ctx.body = {
+          code: 400,
+          msg: "删除失败",
+        };
+      }
+    })
+    .catch((error) => {
+      console.error("删除出现异常", error);
+    });
+});
 module.exports = router;
